42a5aa4dabddbeb305989573579f227d3909c0b0
cesar pagliacci <pagliaccicesar@gmail.com>
2023-10-04 17:41:41 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    /* Navigation smooth scroll */
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top-30
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+
+    /* Change menu background on scrolling */
+    $(document).scroll(function() {
+        if ($(document).scrollTop() > 100) {
+            $("nav").addClass("navbar-scrolled");
+        } else {
+            $("nav").removeClass("navbar-scrolled");
+        }
+    });
+});
+
+
+/*   galeria de remeras    */
+
+//Producto a favoritos
+$('.card .aFavs').click(function(){
+  $(this).parents('.card').toggleClass('esFav');
+})
+//Producto al carrito
+$('.card .alCarrito').click(function(){
+  $(this).parents('.card').toggleClass('enCarrito');
+})
+
+
+
+
